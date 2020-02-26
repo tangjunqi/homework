a89188a8191b13e9a7d2f1bd12de523308260293
tangjunqi <2018302120117@whu.edu.cn>
2020-02-26 14:51:56 +0800

    commit Homework2

---
+++ Homework2/project2/project4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace project4
+{
+    class Program
+    {
+        static void Information(int[] a, out int max, out int min, out int total, out double average)
+        {
+            max = 0;                                
+            min = 10000;
+            total = 0;
+            for (int i = 0; i < 10; i++)        //求出最大值，最小值，平均值，总数
+            {
+                if (max < a[i])
+                { max = a[i]; }
+                if (min > a[i])
+                { min = a[i]; }
+                total += a[i];
+            }
+            average = total / 10.0;
+
+        }
+        static void Main(string[] args)
+        {
+            int max, min,total;      //定义所要求的四个变量及数组
+            double average;
+            int[] array=new int[10];
+
+            Console.Write("请输入10个数组元素的值\n");   //初始化数组
+            for (int i = 0; i<10; i++)
+            {
+                array[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Information(array,out max,out min,out total,out average);   
+            Console.Write("最大值:{0}最小值:{1}平均值:{2} 总值:{3}",max,min,average,total);
+          
+        }
+    }
+}
---
+++ Homework2/project3/project5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[101];     //创建整数数组，并初始化，以0标准为素数
+            for (int i = 1; i < 101; i++)
+                number[i] = 0;
+           
+            for (int i = 2; i <= 50; i++)    //筛选，剔除非素数
+                for (int j = 2; i * j <= 100; j++)
+                    number[i * j] = 1;
+
+            for (int i = 2; i < 101; i++)    //显示素数
+                if (number[i] == 0)
+                { Console.WriteLine(i); }           
+        }
+    }
+}
