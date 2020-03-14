544c66863c6849f26d43aeb09ea220e892b23712
tangjunqi <2018302120117@whu.edu.cn>
2020-03-14 16:47:40 +0800

    -mcommit Homework4

---
+++ Homework4/project8/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project8
+
+{    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void Foreach(Action<T> action)
+        {
+            for (Node<T> node = this.Head;node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int min = 0;
+            int total = 0;
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            intlist.Foreach(s => Console.WriteLine(s));
+            intlist.Foreach(s => { max = max > s ? max : s;});
+            Console.WriteLine($"最大值是{max}");
+            intlist.Foreach(s => { min = min < s ? min : s; });
+            Console.WriteLine($"最小值是{min}");
+            intlist.Foreach(s => total+=s);
+            Console.WriteLine($"总数是{total}");
+        }
+    }
+}
---
+++ Homework4/project9/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+
+namespace project9
+{
+    public delegate void TickHandler(object sender, TickEventArgs args);
+    public class TickEventArgs{}
+
+    public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+    public class AlarmEventArgs { }
+    public class Clock
+    {
+        public event TickHandler Onclick1;
+        public event AlarmHandler Onclick2;
+        public int sethour;
+        public int setminute;
+        public int setsecond;
+        public Clock(int hour,int minute,int second)
+        {
+            sethour = hour;
+            setminute = minute;
+            setsecond = second;
+        }
+
+        public void Tick()
+        {
+            TickEventArgs args = new TickEventArgs();
+            Onclick1(this, args);
+        }
+        public void Alarm()
+        {
+            AlarmEventArgs args = new AlarmEventArgs();
+            Onclick2(this, args);
+        }
+
+        public void work()
+        {
+            while (true)
+            {
+                Tick();
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                if (hour == sethour && minute == setminute && second == setsecond)
+                {
+                    Alarm();
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock A = new Clock(0,0,0);
+            A.Onclick1 += ClockTick;
+            A.Onclick2 += ClockAlarm;
+            A.work();
+        }
+        static public void ClockTick(object sender, TickEventArgs args)
+        {
+            Console.WriteLine("时间转动1秒");
+        }
+        static public void ClockAlarm(object sender, AlarmEventArgs args)
+        {
+            Console.WriteLine("闹钟响起");
+        }
+    }
+}
